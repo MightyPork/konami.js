da1234ef0a329fbaa1648cf1f5bea27324e19127
MightyPork <ondra@ondrovo.com>
2015-04-10 19:40:17 +0200

    added code

---
+++ konami.js
@@ -0,0 +1,31 @@
+(function (window) {
+    "use strict";
+
+    var d = window.document;
+    var keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var progress = 0;
+
+    window.konami = function (h) {
+        var listener = function (e) {
+            if (e.keyCode == keys[progress]) {
+                if (++progress == keys.length) {
+                    console.log('コナミ');
+
+                    if (typeof h == 'function') {
+                        h();
+                    }
+
+                    progress = 0;
+                }
+            } else {
+                progress = 0;
+            }
+        };
+
+        if (d.addEventListener) {
+            d.addEventListener('keyup', listener);
+        } else {
+            d.onkeyup = listener;
+        }
+    };
+})(window);
